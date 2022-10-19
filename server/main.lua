b757d10dae48c6c43824e8b131dbd7db06d1a744
Bryan <74470238+BryanDevelopes@users.noreply.github.com>
2022-10-18 21:47:28 -0400

    Update to support saving into garage + optional job restriction

--- server/main.lua
+++ server/main.lua
@@ -6,10 +6,41 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 for k, v in pairs(Config.ChameleonColors) do
     ESX.RegisterUsableItem('chameleonpaint_'..k, function(source, item)
-        print("Item Used..")
         local src = source
         local xPlayer = ESX.GetPlayerFromId(src)
-        TriggerClientEvent('chameleonpaint:sprayVehicle', src , item.name, k)
-        xPlayer.removeInventoryItem('chameleonpaint_'..k, 1)
+        if Config.JobOnly then
+            local playerjob = xPlayer.getJob().name
+            if playerjob == Config.JobName then
+                TriggerClientEvent('chameleonpaint:sprayVehicle', src , item.name, k)
+                xPlayer.removeInventoryItem('chameleonpaint_'..k, 1)
+            else
+                TriggerClientEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You dont have permission to use this!'})
+            end
+        else
+            TriggerClientEvent('chameleonpaint:sprayVehicle', src , item.name, k)
+            xPlayer.removeInventoryItem('chameleonpaint_'..k, 1)
+        end
     end)
 end
+
+
+RegisterServerEvent('chameleonpaint:updateVehicle')
+AddEventHandler('chameleonpaint:updateVehicle', function(vehicleProps)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	MySQL.Async.fetchAll('SELECT vehicle FROM owned_vehicles WHERE plate = @plate', {
+		['@plate'] = vehicleProps.plate
+	}, function(result)
+		if result[1] then
+			local vehicle = json.decode(result[1].vehicle)
+
+			if vehicleProps.model == vehicle.model then
+				MySQL.Async.execute('UPDATE owned_vehicles SET vehicle = @vehicle WHERE plate = @plate', {
+					['@plate'] = vehicleProps.plate,
+					['@vehicle'] = json.encode(vehicleProps)
+				})
+			else
+				print(('esx_customs: %s attempted to upgrade vehicle with mismatching vehicle model!'):format(xPlayer.identifier))
+			end
+		end
+	end)
+end)
